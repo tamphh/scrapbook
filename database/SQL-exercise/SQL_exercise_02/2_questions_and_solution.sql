d16815c51fe67c74a8457b8277a514e1683c2031
tamphh <tampham@tinypulse.com>
2019-02-11 15:37:45 +0700

    Update 2_questions_and_solution.sql

--- database/SQL-exercise/SQL_exercise_02/2_questions_and_solution.sql
+++ database/SQL-exercise/SQL_exercise_02/2_questions_and_solution.sql
@@ -47,12 +47,40 @@ select * from Departments
   where Budget > (
     select avg(Budget) from Departments
   );
-  
+    
 -- 2.14 Select the names of departments with more than two employees.
+select d.Name
+from Employees e
+  inner join Departments d on d.Code = e.Department
+group by Department
+having count(e.Department) > 2;
+
 -- 2.15 Very Important - Select the name and last name of employees working for departments with second lowest budget.
+-- ref: https://www.programmerinterview.com/index.php/database-sql/find-nth-highest-salary-sql/
+select e.Name, e.LastName
+  from Employees e
+  inner join Departments d on d.Code = e.Department
+  where d.budget = (
+      select Budget from Departments
+      order by Budget ASC
+      limit 1,1
+    );
+
+/* With subquery */
+SELECT e.Name, e.LastName
+FROM Employees e 
+WHERE e.Department = (
+       SELECT sub.Code 
+       FROM (SELECT * FROM Departments d ORDER BY d.budget LIMIT 2) sub 
+       ORDER BY budget DESC LIMIT 1);
+       
 -- 2.16  Add a new department called "Quality Assurance", with a budget of $40,000 and departmental code 11. 
 -- And Add an employee called "Mary Moore" in that department, with SSN 847-21-9811.
+
 -- 2.17 Reduce the budget of all departments by 10%.
+
 -- 2.18 Reassign all employees from the Research department (code 77) to the IT department (code 14).
+
 -- 2.19 Delete from the table all employees in the IT department (code 14).
+
 -- 2.20 Delete from the table all employees who work in departments with a budget greater than or equal to $60,000.
