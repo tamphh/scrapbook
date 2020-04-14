21a2c128a26227071e623a741aa9f0d8c3852341
Tam Pham <tampham@tinypulse.com>
2020-04-14 20:49:12 +0700

    Update DB exercise

--- database/SQL-exercise/SQL_exercise_04/4_questions_and_solutions.sql
+++ database/SQL-exercise/SQL_exercise_04/4_questions_and_solutions.sql
@@ -18,11 +18,34 @@ left join Movies m on m.code = mt.movie
 where movie is not null;
 
 -- 4.6 Select all data from all movies and, if that movie is being shown in a theater, show the data from the theater.
+select * from Movies m
+  left join MovieTheaters mt
+    on mt.Movie = m.code
+order by m.code;
 
 -- 4.7 Show the titles of movies not currently being shown in any theaters.
+select * from Movies m
+  left join MovieTheaters mt
+    on mt.Movie = m.code
+where mt.code is null
+order by m.code;
 
 -- 4.8 Add the unrated movie "One, Two, Three".
+insert into Movies(Code, Title, Rating) values(9, 'One, Two, Three', null);
 
 -- 4.9 Set the rating of all unrated movies to "G".
+update Movies
+set Rating = 'G'
+where Movies.Code in (
+  select m.Code from (select * from Movies) m where m.Rating is null
+);
+
+UPDATE Movies
+SET Rating = 'G'
+WHERE Rating is NULL;
 
 -- 4.10 Remove movie theaters projecting movies rated "NC-17".
+delete from MovieTheaters
+where MovieTheaters.Movie in (
+  select m.Code from (select * from Movies) m where m.Rating='NC-17'
+);
